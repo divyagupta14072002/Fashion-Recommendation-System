e3b9df9c0ede0bf4b75b5211cf523c05a170b84c
Divya Gupta <121746281+divyagupta14072002@users.noreply.github.com>
2023-07-18 16:35:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import tensorflow
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.layers import GlobalMaxPooling2D
-from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
+from tensorflow import keras
+from keras.preprocessing import image
+from keras.layers import GlobalMaxPooling2D
+from keras.applications.resnet import ResNet50,preprocess_input
 import numpy as np
 from numpy.linalg import norm
 import os
